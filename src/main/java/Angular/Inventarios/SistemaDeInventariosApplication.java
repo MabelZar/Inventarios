1d32e5e5f092bda9bcc098c8b09d1d524acf1787
MabelZar <mabel.zar.esp@gmail.com>
2025-02-04 19:59:17 +0100

    Fist commit

---
+++ src/main/java/Angular/Inventarios/SistemaDeInventariosApplication.java
@@ -0,0 +1,13 @@
+package Angular.Inventarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeInventariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeInventariosApplication.class, args);
+	}
+
+}
